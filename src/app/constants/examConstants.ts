7ae0a90b591cd85b4dbb7518dd7cbf97bc6810e7
dvirberlo <dvir.berl@gmail.com>
2023-09-15 15:39:58 +0300

    fix: hot fix: number of question in exam

--- src/app/constants/examConstants.ts
+++ src/app/constants/examConstants.ts
@@ -1,5 +1,3 @@
-// export const EXAM_QUESTIONS = 30;
-export const EXAM_QUESTIONS = 3;
+export const EXAM_QUESTIONS = 30;
 export const EXAM_TIME = 40 * 60 * 1000;
-// export const MINIMUM_PASSING_QUESTION_COUNT = 26;
-export const MINIMUM_PASSING_QUESTION_COUNT = 2;
+export const MINIMUM_PASSING_QUESTION_COUNT = 26;
